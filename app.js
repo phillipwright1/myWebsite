cc79084d77efa6261802938a94d6163d1b51ab67
phillip wright <bootsie8917@gmail.com>
2014-10-16 11:50:06 -0700

    initial commit for website

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = angular.module('personalPage', ['ui.router']);
+app.config(function($stateProvider) {
+       // $urlRouteProvider.otherwise("/index");
+
+        $stateProvider
+        .state("index",{
+            url:"/index",
+            templateUrl:"/index.html"
+        })
+        .state("blog1",{
+            url: "/blog1",
+            templateUrl: "/blog1.html"
+        })
+        .state("blog2", {
+        url: "/blog2",
+        templateUrl: "/blog2.html"
+        })
+        .state("blog3",{
+            url:"/blog3",
+        templateUrl: "/blog3.html"
+
+
+    })
+});
+
+
+
+
